1c0498ae54b4aa4884f9f117235adfedb0885146
Utkarsh Singh <61621292+utkarshsingh20520@users.noreply.github.com>
2021-07-03 15:37:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,54 +1,93 @@
 window.onload=function(){
+	
 
 var open = document.getElementById("opennav");
 var close = document.getElementById("closenav");
 var nav = document.getElementById("collapsena");
 var back = document.getElementById("Background");
+var backs = document.getElementById("Mainback");
 
 open.addEventListener("click",opened = () =>{
    nav.style.width = "370px";
    back.style.display = "block";
+   backs.style.width = "calc(100% - 370px)";
 });
 
 close.addEventListener("click",closed = () =>{
    nav.style.width = "0px";
    back.style.display = "none";
+   backs.style.width = "100%";
 });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
+
+// var nav31 = document.getElementById("n31");
+// var nav32 = document.getElementById("n32");
+// var nav33 = document.getElementById("n33");
+// var nav34 = document.getElementById("n34");
+// var nav35 = document.getElementById("n35");
+
+// nav31.addEventListener('click',show(0));
+// nav32.addEventListener('click',show(1));
+// nav33.addEventListener('click',show(2));
+// nav34.addEventListener('click',show(3));
+// nav35.addEventListener('click',show(4));
+
+// var arr = ["n31","n32","n33","n34","n35"];
+
+// function show(j){
+// 	for(let i=0;i<5;i++)
+// 	{
+// 		if(j==i){
+// 	      document.getElementById(arr[i]).style.borderBottom = "2px solid blue";
+// 	      console.log(j);
+// 	    }
+// 	    else{
+// 	      document.getElementById(arr[i]).style.borderBottom = "0px solid blue";
+// 	      console.log(i);	
+// 	    }
+//     }
+// }
+
+
+var chart = new CanvasJS.Chart("chartContainer", {
+
+
+  
+	data: [{
+		type: "doughnut",
+		startAngle: 60,
+		//innerRadius: 60,
+		/* indexLabelFontSize: 17,
+		indexLabel: "{label} - #percent%",
+		toolTipContent: "<b>{label}:</b> {y} (#percent%)", */
+		dataPoints: [
+			{ y: 3, label: "",color: "#595959" },
+      { y: .1,color: "white" },
+			{ y: 5, label: "",color:"#8c8c8c"},
+       { y: .1, color: "white" },
+			{ y: 22, label: "",color: "grey" },
+       { y: .1,color: "white" },
+			{ y: 3, label: "",color: "#B0B0B0"},
+       { y: .1, color: "white" },
+			{ y: 6, label: "",color: "#660000"},
+       { y: .1,color: "white" },
+			{ y: 13, label: "",color:"#b38600"},
+       { y: .1,color: "white" },
+      { y: 22, label: "",color: "#1a0000" },
+       { y: .1,color: "white" },
+			{ y: 3, label: "",color:"blue" },
+       { y: .1,color: "white" },
+			{ y: 5, label: "",color:"lightblue" },
+       { y: .1, color: "white" },
+			{ y: 20, label: "",color:"#99e6ff"},
+       { y: .1,color: "white" },
+			
+      
+      
+		]
+	}]
 });
+  
+chart.render();
 
 }
